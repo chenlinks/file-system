c5828341ee05e2fd719097e07d889d03fc8905c8
chenling <chenling@deepexi.com>
2020-01-21 21:11:56 +0800

    code init

---
+++ file-system-api/src/main/java/com/file/system/FileSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.file.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileSystemApiApplication.class, args);
+    }
+
+}
---
+++ file-system-provider/src/main/java/com/file/system/FileSystemProviderApplication.java
@@ -0,0 +1,13 @@
+package com.file.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSystemProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileSystemProviderApplication.class, args);
+    }
+
+}
